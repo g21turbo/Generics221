df76ea8e304623c439e3b991ff41470d90269073
User <dg21stew@gmail.com>
2022-09-12 15:04:00 -0700

    comments and output update

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,40 @@ public class Main {
         BaseballPlayer joe = new BaseballPlayer("joe");
         SoccerPlayer jim = new SoccerPlayer("jim");
 
+        //Changed type, list only accepts football players
+        Team<FootballPlayer> americanEagles = new Team<>("American Eagles");
 
-        Team americanEagles = new Team("American Eagles");
         americanEagles.addPlayer(bob);
-        americanEagles.addPlayer(joe);
-        americanEagles.addPlayer(jim);
+//        americanEagles.addPlayer(joe);
+//        americanEagles.addPlayer(jim);
+
         System.out.println(americanEagles.numPlayers() + " players on the team.");
 
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(joe);
 
+
+        Team<SoccerPlayer> brokeTeam = new Team<>("United");
+        brokeTeam.addPlayer(jim);
+
+        Team<FootballPlayer> cowboys = new Team<>("Dallas Cowboys");
+        FootballPlayer aikman = new FootballPlayer("Troy Aikman");
+        FootballPlayer smith = new FootballPlayer("Emmitt Smith");
+        FootballPlayer irvin = new FootballPlayer("Michael Irvin");
+
+        Team<FootballPlayer> niners = new Team<>("San Francisco 49ers");
+        FootballPlayer young = new FootballPlayer("Steve Young");
+        FootballPlayer rice = new FootballPlayer("Jerry Rice");
+        FootballPlayer sanders = new FootballPlayer("Deon Sanders");
+
+        Team<FootballPlayer> practiceSquad = new Team<>("Cowboys Practice Squad");
+        FootballPlayer moose = new FootballPlayer("Daryl Johnston");
+        FootballPlayer novacek = new FootballPlayer("Jay Novacek");
+        FootballPlayer holt = new FootballPlayer("Issaiac Holt");
+
+        cowboys.matchResult(niners, 24, 17);
+        niners.matchResult(cowboys, 0, 47);
+        cowboys.matchResult(practiceSquad, 1, 1);
 
 
     }
--- src/Team.java
+++ src/Team.java
@@ -1,7 +1,9 @@
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class Team {
+
+//T is a bounded type parameter, all types must use parameters in Player class
+public class Team <T extends Player> {
 
     private String name;
     int played = 0;
@@ -9,17 +11,22 @@ public class Team {
     int lost = 0;
     int tied = 0;
 
-    private ArrayList<Player> members = new ArrayList<>();
 
+    //ArrayList for Team members
+    private ArrayList<T> members = new ArrayList<>();
+
+    //Constructor
     public Team(String name) {
         this.name = name;
     }
 
+    //Getter
     public String getName() {
         return name;
     }
 
-    public boolean addPlayer(Player player) {
+    //Add a player
+    public boolean addPlayer(T player) {
         if (members.contains(player)) {
             System.out.println(player.getName() + " is already on the team");
             return false;
@@ -30,20 +37,31 @@ public class Team {
         }
     }
 
+    //Returns the size of the team(int)
     public int numPlayers() {
         return this.members.size();
     }
 
+    //Determines match results based off passed scores
     public void matchResult(Team opponent, int ourScore, int theirScore) {
+
+        String message;
+
         if (ourScore > theirScore) {
             won++;
+            message = " beat the ";
+
         } else if (ourScore == theirScore) {
             tied++;
+            message = " tied with the ";
+
         } else {
             lost++;
+            message = " lost to the ";
         }
         played++;
         if (opponent != null) {
+            System.out.println(this.getName() + message + opponent.getName());
             opponent.matchResult(null, theirScore, ourScore);
         }
     }
